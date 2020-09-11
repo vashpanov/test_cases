81fdee27a5fded60d508e2c8008032d57d0e68fe
vashpanov <43466074+vashpanov@users.noreply.github.com>
2020-09-11 15:53:56 +0300

    Update case3.py
    Добавлен монитор памяти

--- case3.py
+++ case3.py
@@ -5,17 +5,29 @@ import pandas as pd
 
 input = 'input.txt'
 output = 'output.txt'
-arr=[]
-chunker = pd.read_table(input, chunksize=10000)
+arr = []
+chunker = pd.read_table(input, chunksize=1000)
 
 for chunk in chunker:
-    chunkArr=chunk.to_numpy()
+    chunkArr = chunk.to_numpy()
+    print(chunk.info(memory_usage='deep'))
     for i in range(len(chunkArr)):
         if not chunkArr[i] in arr:
             arr.append(chunkArr[i].tolist()[0])
             print(arr)
 writer = open(output, "w")
 
+def mem_usage(pandas_obj):
+    if isinstance(pandas_obj,pd.DataFrame):
+        usage_b = pandas_obj.memory_usage(deep=True).sum()
+    else:
+        usage_b = pandas_obj.memory_usage(deep=True)
+    usage_mb = usage_b / 1024 ** 2
+    return "{:03.2f} MB".format(usage_mb)
+
+
+
+
 for i in range(len(arr)):
-    writer.write(arr[i]+"\n")
+    writer.write(arr[i] + "\n")
 writer.close()
